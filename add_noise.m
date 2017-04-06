ea71ba9335931d0a74e2904645f243552efa52f4
whitephenix13 <alexandrelasbleis@yahoo.fr>
2017-04-06 16:44:54 +0200

    added the add noise function

--- add_noise.m
+++ add_noise.m
@@ -1,5 +1,5 @@
-function [ noised_point_cloud ] = add_noise( point_cloud )
+function [ noised_point_cloud ] = add_noise( point_cloud, sigma)
 %add noise to the point cloud to test the stability 
-
+noised_point_cloud= point_cloud + sigma * randn(size(point_cloud));
 end
 
