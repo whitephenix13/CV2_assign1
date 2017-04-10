a6f1d0bc93bed22e8e73726cbbd9ba481e700851
Vladimir <vladimir.isakov91@yandex.com>
2017-04-10 13:07:32 +0200

    Point Cloud
    Outputs the point cloud.

--- Preprocess.m
+++ Preprocess.m
@@ -1,8 +1,9 @@
-function [ C ] = Preprocess( img, mask, method )
+function [ PC ] = Preprocess( img, mask, depth, method )
 
 %Load image and mask
-img = imread(img);
-mask = imread(mask);
+img = uint16(imread(img));
+mask = uint16(imread(mask));
+depth = imread(depth);
 
 %make a better mask from the original
 mask(mask==255)=1;
@@ -10,9 +11,9 @@ mask(mask==255)=1;
 %Remove the background
 %Use 'my'
 if strcmp(method, 'my')
-    K1 = img(:,:,1).*mask;
-    K2 = img(:,:,2).*mask;
-    K3 = img(:,:,3).*mask;
+    K1 = img(:,:,1).*mask.*depth;
+    K2 = img(:,:,2).*mask.*depth;
+    K3 = img(:,:,3).*mask.*depth;
     res = cat(3,K1,K2,K3);   
 elseif strcmp(method, 'falsecolor')
     %Fuse image and mask using falsecolor method
@@ -32,10 +33,31 @@ I = 0.21*res(:,:,1) + 0.72*res(:,:,2) + 0.07*res(:,:,3);
 %Find corners
 C = corner(I);
 
+[~, ordered]= run(depth);
+
+X = zeros(length(C(:,1)),1);
+Y = zeros(length(C(:,1)),1);
+Z = zeros(length(C(:,1)),1);
+
+for i=1:length(C(:,1))
+    X(i,1) = ordered(C(i,2),C(i,1),1);
+    Y(i,1) = ordered(C(i,2),C(i,1),2);
+    Z(i,1) = ordered(C(i,2),C(i,1),3);
+end
+
+X = X(find(X~=0));
+Y = Y(find(Y~=0));
+Z = Z(find(Z~=0));
+
+PC = [X Y Z];
+
 %Plots
-% figure(1);
-% subplot(1,2,1); imshow(res);
-% subplot(1,2,2); imshow(res); hold on; plot(C(:,1), C(:,2), 'r*');
+figure(1);
+subplot(1,3,1); imshow(res);
+subplot(1,3,2); imshow(res); hold on; plot(C(:,1), C(:,2), 'r*');
+subplot(1,3,3); imshow(depth); hold on; plot(C(:,1), C(:,2), 'r*');
+figure(2);
+scatter3(X,Y,Z);
 
 end
 
